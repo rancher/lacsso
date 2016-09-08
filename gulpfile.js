85b50ba6e610566d5bc3397e54028c3fec9cebbc
Westly Wright <westly.wright@gmail.com>
2016-09-08 12:15:56 -0700

    update gulp releasing

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,16 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var git = require('gulp-git');
 var bump = require('gulp-bump');
 var filter = require('gulp-filter');
-var tag_version = require('gulp-tag-version');
+var release = require('gulp-github-release');
 var path = require("path");
 
-gulp.task('sass', function () {
-  return gulp.src('./lacsso.scss')
-    .pipe(sass({ includePaths: ['./**'] }))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./**/*.scss', ['sass']);
-});
-
-gulp.task('tag',       function() { return inc('patch'); })
-gulp.task('tag-patch', function() { return inc('patch'); })
-gulp.task('tag-minor', function() { return inc('minor'); })
-gulp.task('tag-major', function() { return inc('major'); })
+gulp.task('release',       function() { return inc('patch'); })
+gulp.task('release-patch', function() { return inc('patch'); })
+gulp.task('release-minor', function() { return inc('minor'); })
+gulp.task('release-major', function() { return inc('major'); })
 
 function inc(importance) {
   // get all the files to bump version in
@@ -35,6 +23,10 @@ function inc(importance) {
     .pipe(git.commit('Bump release'))
     // read only one file to get the version number
     .pipe(filter('package.json'))
-    // **tag it in the repository**
-    .pipe(tag_version());
+    // publish the release
+    .pipe(gulp.src('./dist/assets/lacsso.css'))
+    .pipe(release({
+      token: process.env.GITHUB_TOKEN,
+      manifest: require('./package.json')
+    }))
 }
