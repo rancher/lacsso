4443d530d8321a834d507ff381658a431f264664
Westly Wright <westly.wright@gmail.com>
2016-09-19 14:56:14 -0700

    work for bulk header actions

--- addon/components/sortable-table.js
+++ addon/components/sortable-table.js
@@ -3,7 +3,7 @@ import layout from '../templates/components/sortable-table';
 import Sortable from '../mixins/sortable';
 import StickyHeader from '../mixins/sticky-table-header';
 
-const {set} = Ember;
+const {set, get} = Ember;
 
 export default Ember.Component.extend(Sortable, StickyHeader, {
   layout,
@@ -13,12 +13,59 @@ export default Ember.Component.extend(Sortable, StickyHeader, {
   headers: null,
   sortableContent: Ember.computed.alias('body'),
   selectAll: false,
-  setAllSelected: Ember.observer('selectAll', function() {
-    let selectAll = this.get('selectAll');
-    let tableData = this.get('body');
-    this.set('body', tableData.map((item) => {
-      set(item, 'selected', selectAll);
-      return item;
-    }));
-  }),
+  availableActions: null,
+  selectedNodes: [],
+  actions: {
+    selectUnselect: function(node) {
+      let selectedNodes = this.get('selectedNodes');
+      let indexOfNode = -1;
+
+      let nodeExists = selectedNodes.find((item, index) => {
+        if (node === item) {
+          indexOfNode = index;
+          return true;
+        }
+        indexOfNode = -1;
+        return false;
+      });
+
+      if (nodeExists) {
+        selectedNodes.splice(indexOfNode, 1);
+      } else {
+        selectedNodes.push(node);
+      }
+    },
+    selectUnselectAll: Ember.observer('selectAll', function() {
+      console.log(this.get('body'));
+      let selectAll = this.get('selectAll');
+      if (selectAll) {
+        selectAll = this.set('selectAll', false);
+        this.set('selectedNodes', []);
+      } else {
+        selectAll = this.set('selectAll', true);
+        this.set('selectedNodes', this.get('body'));
+        this.mergeAvailableActions(this.get('body'));
+      }
+    }),
+  },
+  mergeAvailableActions: function(data) {
+    let commonActions = {};
+    data.forEach((item, idx) => {
+      let actionKeys = Object.keys(get(item, 'actions'));
+      if (idx === 0) {
+        let temp = {};
+        actionKeys.forEach((key) => {
+          temp[key] = [get(item.actions, key)];
+        });
+        commonActions = temp;
+        return;
+      }
+
+      actionKeys.forEach((key) => {
+        if (commonActions[key]) {
+          commonActions[key].push(get(item.actions, key));
+        }
+      });
+    });
+  }
 });
