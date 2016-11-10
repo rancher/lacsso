4685f02e30cb40c1591c7d42bba69d7588072dda
Vincent Fiduccia <vincent@rancher.com>
2016-11-10 12:12:52 -0700

    Save scroll position when showing modal

--- addon/services/modal.js
+++ addon/services/modal.js
@@ -4,15 +4,25 @@ export default Ember.Service.extend({
   modalType: 'generic-modal',
   modalOpts: null,
   modalVisible: false,
+  lastScroll: null,
   closeWithOutsideClick: Ember.computed.alias('modalOpts.closeWithOutsideClick'),
   toggleModal: function(type=null, opts=null) {
     if (opts) {
       this.set('modalOpts', opts);
     }
+
     this.set('modalType', type);
-    this.toggleProperty('modalVisible');
-    if (!this.get('modalVisible') && this.get('modalOpts')) {
+
+    if ( this.get('modalVisible') ) {
+      this.set('modalVisible', false);
       this.set('modalOpts', null);
+      Ember.run.next(() => {
+        document.body.scrollTop = this.get('lastScroll');
+      });
+    } else {
+      this.set('lastScroll', document.body.scrollTop);
+      this.set('modalVisible', true);
+      document.body.scrollTop = 0;
     }
   },
 });
